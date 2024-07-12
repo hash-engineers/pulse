5c4c49053163f0777bf41861eb6c224007344d25
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-13 04:11:14 +0600

    feat: handle prisma client validation error

---
+++ backend/src/errors/handle-prisma-client-validation-error.ts
@@ -0,0 +1,16 @@
+import { Prisma } from '@prisma/client';
+import { GenericErrorResponse } from '../interfaces/common';
+
+const handlePrismaClientValidationError = (
+  error: Prisma.PrismaClientValidationError,
+): GenericErrorResponse => {
+  const errors = [{ path: '', message: error.message }];
+
+  return {
+    statusCode: 400,
+    message: 'prisma client validation error!',
+    errorMessages: errors,
+  };
+};
+
+export default handlePrismaClientValidationError;
