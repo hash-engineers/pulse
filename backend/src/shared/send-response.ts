8a9e972f9f5bfa9c219e58272c8e50f69931b3e0
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-13 03:47:34 +0600

    feat: add send response function
    a reusable function for send response in a same structure

---
+++ backend/src/shared/send-response.ts
@@ -0,0 +1,27 @@
+import { Response } from 'express';
+import { Meta } from '../interfaces/common';
+
+interface ApiResponse<T> {
+  statusCode: number;
+  success: boolean;
+  message?: string | null;
+  meta?: Meta;
+  data?: T | null;
+}
+
+const sendResponse = <T>(
+  res: Response,
+  { statusCode, success, message, meta, data }: ApiResponse<T>,
+): void => {
+  const responseData: ApiResponse<T> = {
+    statusCode,
+    success,
+    message: message ?? null,
+    meta: meta ?? null ?? undefined,
+    data: data ?? null ?? undefined,
+  };
+
+  res.status(statusCode).json(responseData);
+};
+
+export default sendResponse;
