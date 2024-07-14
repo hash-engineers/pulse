0e2bf6cdefff709e9bcb87fd223de56745da7caa
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-14 20:58:04 +0600

    feat: add downtime blink component
    and change the color of uptime blink component

--- frontend/app/dashboard/monitors/status.tsx
+++ frontend/app/dashboard/monitors/status.tsx
@@ -1,9 +1,11 @@
+import { DowntimeBlinking } from '@/components/downtime-blinking';
 import { UptimeBlinking } from '@/components/uptime-blinking';
 
 export function Status() {
   return (
     <div>
       <UptimeBlinking />
+      <DowntimeBlinking />
     </div>
   );
 }
--- frontend/components/uptime-blinking.tsx
+++ frontend/components/uptime-blinking.tsx
@@ -1,8 +1,8 @@
 export function UptimeBlinking() {
   return (
     <div className="size-8 relative">
-      <div className="absolute top-1/3 left-1/3 animate-pulse bg-teal-400 size-3 rounded-full z-10" />
-      <div className="animate-pulse bg-green-700/50 size-8 rounded-full" />
+      <div className="absolute top-1/3 left-1/3 animate-pulse bg-green-600 size-3 rounded-full z-10" />
+      <div className="animate-pulse bg-teal-400/20 size-8 rounded-full" />
     </div>
   );
 }
