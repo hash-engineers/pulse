5ceef0dccb47584b40531ee76c6d0484aaa9749a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-14 20:11:43 +0600

    feat: add monitor page

---
+++ frontend/app/dashboard/monitors/page.tsx
@@ -0,0 +1,3 @@
+export default function Monitors() {
+  return <div>Monitors</div>;
+}
--- frontend/components/layout/auth-or-dashboard.tsx
+++ frontend/components/layout/auth-or-dashboard.tsx
@@ -1,16 +1,17 @@
+import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { Button } from '../ui/button';
 
 type Props = { className?: string };
 
 export function AuthOrDashboard({ className }: Props) {
-  const user = false; // LATTER: IT WILL COME FROM DB
+  const user = true; // LATTER: IT WILL COME FROM DB
 
   return (
     <div className={cn('hidden md:block', className)}>
       {user ? (
         <Button size="sm" variant="secondary">
-          Dashboard
+          <Link href="/dashboard/monitors">Dashboard</Link>
         </Button>
       ) : (
         <Button size="sm">Sign In</Button>
