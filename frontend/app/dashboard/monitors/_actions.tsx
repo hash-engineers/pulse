a999c0f0b03bbe7a6fd43dbaf69e68140c93697a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-14 22:06:59 +0600

    feat: add some action for monitor control

---
+++ frontend/app/dashboard/monitors/_actions.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/components/ui/button';
+import { Pause, Send, Settings, ShieldAlert } from 'lucide-react';
+
+export function Actions() {
+  return (
+    <div className="text-muted-foreground flex items-center justify-start gap-x-2">
+      <Button
+        variant="ghost"
+        size="sm"
+        className="flex items-center justify-center gap-x-2"
+      >
+        <Send size="16" /> <span>Send a test alert</span>
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="sm"
+        className="flex items-center justify-center gap-x-2"
+      >
+        <ShieldAlert size="16" /> <span>Incidents</span>
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="sm"
+        className="flex items-center justify-center gap-x-2"
+      >
+        <Pause size="16" /> <span>Pause</span>
+      </Button>
+
+      <Button
+        variant="ghost"
+        size="sm"
+        className="flex items-center justify-center gap-x-2"
+      >
+        <Settings size="16" /> <span>Configure</span>
+      </Button>
+    </div>
+  );
+}
--- frontend/app/dashboard/monitors/page.tsx
+++ frontend/app/dashboard/monitors/page.tsx
@@ -1,9 +1,11 @@
 import { Status } from './_status';
+import { Actions } from './_actions';
 
 export default function Monitors() {
   return (
     <div className="p-2">
       <Status />
+      <Actions />
     </div>
   );
 }
