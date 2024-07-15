07504b7feb7683aeab0c222acb1d0a6668cbf542
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-15 10:39:37 +0600

    feat: add 3 more column at monitor table
    here use dummy data later on it will come from database

--- frontend/app/dashboard/monitors/_data-table.tsx
+++ frontend/app/dashboard/monitors/_data-table.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import { data, columns } from '@/lib/monitor';
 import {
-  ColumnDef,
   flexRender,
   getCoreRowModel,
   useReactTable,
@@ -14,67 +14,6 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-
-type Payment = {
-  id: string;
-  downtime: string;
-  timePeriod:
-    | 'Today'
-    | 'Last 7 days'
-    | 'Last 30 days'
-    | 'Last 365 days'
-    | 'All Time (Last 3 days)'
-    | 'Since 12 July 2024 untill today';
-  availability: string;
-};
-
-const data: Payment[] = [
-  {
-    id: 'm5gr84i9',
-    downtime: 'none',
-    timePeriod: 'Today',
-    availability: '100.0000%',
-  },
-  {
-    id: '3u1reuv4',
-    downtime: 'none',
-    timePeriod: 'Last 7 days',
-    availability: '100.0000%',
-  },
-  {
-    id: 'derv1ws0',
-    downtime: 'none',
-    timePeriod: 'Last 30 days',
-    availability: '100.0000%',
-  },
-  {
-    id: '5kma53ae',
-    downtime: 'none',
-    timePeriod: 'Last 365 days',
-    availability: '100.0000%',
-  },
-  {
-    id: 'bhqecj4p',
-    downtime: 'none',
-    timePeriod: 'All Time (Last 3 days)',
-    availability: '100.0000%',
-  },
-];
-
-const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: 'timePeriod',
-    header: 'Time Period',
-  },
-  {
-    accessorKey: 'availability',
-    header: 'Availability',
-  },
-  {
-    accessorKey: 'downtime',
-    header: 'Downtime',
-  },
-];
 
 export function DataTable() {
   const table = useReactTable({
---
+++ frontend/lib/monitor.ts
@@ -0,0 +1,94 @@
+import { ColumnDef } from '@tanstack/react-table';
+
+type Payment = {
+  id: string;
+  downtime: string;
+  timePeriod:
+    | 'Today'
+    | 'Last 7 days'
+    | 'Last 30 days'
+    | 'Last 365 days'
+    | 'All Time (Last 3 days)'
+    | 'Since 12 July 2024 untill today';
+  availability: string;
+  incidents: number;
+  longestIncident: string;
+  averageIncident: string;
+};
+
+const data: Payment[] = [
+  {
+    id: 'm5gr84i9',
+    downtime: 'none',
+    timePeriod: 'Today',
+    availability: '100.0000%',
+    incidents: 0,
+    longestIncident: 'none',
+    averageIncident: 'none',
+  },
+  {
+    id: '3u1reuv4',
+    downtime: 'none',
+    timePeriod: 'Last 7 days',
+    availability: '100.0000%',
+    incidents: 0,
+    longestIncident: 'none',
+    averageIncident: 'none',
+  },
+  {
+    id: 'derv1ws0',
+    downtime: 'none',
+    timePeriod: 'Last 30 days',
+    availability: '100.0000%',
+    incidents: 0,
+    longestIncident: 'none',
+    averageIncident: 'none',
+  },
+  {
+    id: '5kma53ae',
+    downtime: 'none',
+    timePeriod: 'Last 365 days',
+    availability: '100.0000%',
+    incidents: 0,
+    longestIncident: 'none',
+    averageIncident: 'none',
+  },
+  {
+    id: 'bhqecj4p',
+    downtime: 'none',
+    timePeriod: 'All Time (Last 3 days)',
+    availability: '100.0000%',
+    incidents: 0,
+    longestIncident: 'none',
+    averageIncident: 'none',
+  },
+];
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'timePeriod',
+    header: 'Time Period',
+  },
+  {
+    accessorKey: 'availability',
+    header: 'Availability',
+  },
+  {
+    accessorKey: 'downtime',
+    header: 'Downtime',
+  },
+  {
+    accessorKey: 'incidents',
+    header: 'Incidents',
+  },
+  {
+    accessorKey: 'longestIncident',
+    header: 'Longest Incident',
+  },
+  {
+    accessorKey: 'averageIncident',
+    header: 'Average Incident',
+  },
+];
+
+export { data, columns };
