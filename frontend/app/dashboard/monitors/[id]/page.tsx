7de38631ae421185e3bd7551c4477f1b61ad0917
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-15 11:14:23 +0600

    refactor: monitor interface move to single monitor path via [id]

---
+++ frontend/app/dashboard/monitors/[id]/page.tsx
@@ -0,0 +1,15 @@
+import { Status } from './_status';
+import { Actions } from './_actions';
+import { KeyInfos } from './_key-infos';
+import { DataTable } from './_data-table';
+
+export default function Monitor() {
+  return (
+    <div className="p-2 space-y-4">
+      <Status />
+      <Actions />
+      <KeyInfos />
+      <DataTable />
+    </div>
+  );
+}
--- frontend/app/dashboard/monitors/page.tsx
+++ frontend/app/dashboard/monitors/page.tsx
@@ -1,14 +1,3 @@
-import { Status } from './_status';
-import { Actions } from './_actions';
-import { KeyInfos } from './_key-infos';
-import { DataTable } from './_data-table';
 export default function Monitors() {
-  return (
-    <div className="p-2 space-y-4">
-      <Status />
-      <Actions />
-      <KeyInfos />
-      <DataTable />
-    </div>
-  );
+  return <div>Monitors</div>;
 }
