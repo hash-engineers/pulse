e945af1bde938017c4db4988b09c55cf15f22ef0
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-16 20:36:45 +0600

    style: the animation on input field
    and delete the a-input file

--- frontend/components/ui/input.tsx
+++ frontend/components/ui/input.tsx
@@ -1,25 +1,57 @@
-import * as React from 'react';
+'use client';
 
 import { cn } from '@/lib/utils';
+import { Div } from '@/lib/motion';
+import { forwardRef, InputHTMLAttributes, useState } from 'react';
+import { useMotionTemplate, useMotionValue } from 'framer-motion';
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const radius = 100;
+    const [visible, setVisible] = useState<boolean>(false);
+
+    let mouseX = useMotionValue(0);
+    let mouseY = useMotionValue(0);
+
+    function handleMouseMove({ currentTarget, clientX, clientY }: any) {
+      let { left, top } = currentTarget.getBoundingClientRect();
+
+      mouseX.set(clientX - left);
+      mouseY.set(clientY - top);
+    }
+
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus:ring-1 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
+      <Div
+        style={{
+          background: useMotionTemplate`
+        radial-gradient(
+          ${
+            visible ? radius + 'px' : '0px'
+          } circle at ${mouseX}px ${mouseY}px, var(--blue-500), transparent 80%
+        )
+      `,
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        className="p-[2px] rounded-lg transition duration-300 group/input"
+      >
+        <input
+          type={type}
+          className={cn(
+            'border border-input bg-background ring-offset-background focus:ring-1 placeholder:text-muted-foreground flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-muted-foreground shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 focus-visible:outline-none focus-visible:ring-[2px focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </Div>
     );
   }
 );
+
 Input.displayName = 'Input';
 
 export { Input };
