6cab124617af36b7b5cffb0d0c78183e4779039f
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-16 10:36:13 +0600

    test: add the demo bento gird in home page

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,10 +1,12 @@
 import { Hero } from './_hero';
+import { BentoGridDemo } from './_info';
 import { Testimonials } from './_testimonials';
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <BentoGridDemo />
       <Testimonials />
     </div>
   );
