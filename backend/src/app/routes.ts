7a7702b47f029a3037f51d9f92fc553085548f32
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-07-27 09:54:59 +0600

    feat: add the monitor route in global route

--- backend/src/app/routes.ts
+++ backend/src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { CompanyRoutes } from './modules/company/company.route';
+import { MonitorRoutes } from './modules/monitor/monitor.route';
 
 const router = Router();
 
-[{ path: '/company', route: CompanyRoutes }].map(({ path, route }) =>
-  router.use(path, route),
-);
+[
+  { path: '/company', route: CompanyRoutes },
+  { path: '/monitor', route: MonitorRoutes },
+].map(({ path, route }) => router.use(path, route));
 
 export const routes = router;
