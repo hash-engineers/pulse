a74a4656cf8ca195b06844c3225ff9250266e5d9
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-23 15:19:34 +0600

    feat: fetch single monitor data from backend

--- frontend/app/dashboard/monitors/[id]/page.tsx
+++ frontend/app/dashboard/monitors/[id]/page.tsx
@@ -1,9 +1,15 @@
+import axios from 'axios';
+import { api } from '@/lib/api';
 import { Status } from './_status';
 import { Actions } from './_actions';
 import { KeyInfos } from './_key-infos';
 import { DataTable } from './_data-table';
 
-export default function Monitor() {
+type Props = { params: { id: string } };
+
+export default async function Monitor({ params: { id } }: Props) {
+  const monitor = await axios.get(`${api}/monitors/${id}`);
+
   return (
     <div className="space-y-4">
       <Status />
