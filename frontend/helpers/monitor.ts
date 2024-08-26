ec0b8c2211f692a471912f07c035e3d97aee2ad8
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-26 14:20:48 +0600

    feat: add calculate monitor availablility helper

---
+++ frontend/helpers/monitor.ts
@@ -0,0 +1,20 @@
+import { Monitor } from '@/types/monitor';
+
+export function calculateMonitorAvailability(monitor: Monitor): string {
+  const createdAt = new Date(monitor.createdAt);
+  const updatedAt = new Date(monitor.updatedAt);
+
+  const totalTime = updatedAt.getTime() - createdAt.getTime();
+
+  const totalDowntime = monitor.incidents.reduce((acc, incident) => {
+    const incidentCreatedAt = new Date(incident.createdAt);
+    const incidentResolvedAt = new Date(incident.resolvedAt);
+    const incidentDowntime =
+      incidentResolvedAt.getTime() - incidentCreatedAt.getTime();
+    return acc + incidentDowntime;
+  }, 0);
+
+  const uptimePercentage = ((totalTime - totalDowntime) / totalTime) * 100;
+
+  return `${uptimePercentage.toFixed(4)}%`;
+}
