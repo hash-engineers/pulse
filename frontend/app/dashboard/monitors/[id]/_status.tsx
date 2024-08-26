7c0a37cf9758d9d8bf44e7fd2803bb7413fdb5b1
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-26 21:27:01 +0600

    fiix: show proper blinking componet as followed by status

--- frontend/app/dashboard/monitors/[id]/_status.tsx
+++ frontend/app/dashboard/monitors/[id]/_status.tsx
@@ -1,17 +1,37 @@
+import { cn } from '@/lib/utils';
 import { Monitor } from '@/types/monitor';
+import { EMonitorStatus } from '@/enums/monitor';
 import { UptimeBlinking } from '@/components/blinks/uptime-blinking';
+import { PausedBlinking } from '@/components/blinks/paused-blinking';
+import { PendingBlinking } from '@/components/blinks/pending-blinking';
+import { DowntimeBlinking } from '@/components/blinks/downtime-blinking';
 
 type Props = Pick<Monitor, 'name' | 'url' | 'status'>;
 
 export function Status({ name, url, status }: Props) {
   return (
     <div className="flex items-center justify-start gap-x-2 p-2 pl-0">
-      <UptimeBlinking />
+      {status === EMonitorStatus.PENDING ? (
+        <PendingBlinking />
+      ) : status === EMonitorStatus.UP ? (
+        <UptimeBlinking />
+      ) : status === EMonitorStatus.PAUSED ? (
+        <PausedBlinking />
+      ) : (
+        <DowntimeBlinking />
+      )}
 
       <div>
         <h4 className="tracking-wide leading-4">{name ?? url}</h4>
         <p className="text-muted-foreground">
-          <span className="text-green-600">
+          <span
+            className={cn({
+              'text-gray-600': status === EMonitorStatus.PENDING,
+              'text-green-600': status === EMonitorStatus.UP,
+              'text-yellow-600': status === EMonitorStatus.PENDING,
+              'text-red-600': status === EMonitorStatus.DOWN,
+            })}
+          >
             {status.slice(0, 1) + status.slice(1, status.length).toLowerCase()}
           </span>{' '}
           <span className="font-extrabold">·</span> Checked erery 3 minutes
