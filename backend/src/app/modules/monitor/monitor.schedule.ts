1f2791c4f0ed98bfc72caa4bb2a862f0c7f34b6c
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-26 21:38:23 +0600

    fix: update the monitor when url is available again

--- backend/src/app/modules/monitor/monitor.schedule.ts
+++ backend/src/app/modules/monitor/monitor.schedule.ts
@@ -18,18 +18,25 @@ const updateMonitorScheduler = () => {
     }
 
     try {
-      await axios.get(monitors[0].url, {
+      const url = await axios.get(monitors[0].url, {
         headers: { 'User-Agent': userAgentHeader },
       });
 
-      console.log('Schedule job works new~');
+      if (url.status === 200 || url.statusText === 'OK') {
+        await prisma.monitor.update({
+          where: { url: monitors[0].url },
+          data: { status: EMonitorStatus.UP, statusCode: url.status },
+        });
+      }
+
+      console.log('Schedule job working.');
     } catch (error: any) {
       console.error('The Error From Monitor Schedule Job ->', error);
 
       if (error) {
         await prisma.monitor.update({
           where: { id: monitors[0].id },
-          data: { status: EMonitorStatus.DOWN },
+          data: { status: EMonitorStatus.DOWN, statusCode: 500 },
         });
       }
     }
