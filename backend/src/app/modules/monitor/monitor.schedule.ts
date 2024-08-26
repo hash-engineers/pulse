6c7e912b0ff75b449e333214bb371ddab427e76c
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-26 21:01:41 +0600

    feat: add update moitor schedule
    to check the status of monitor is it down or up

---
+++ backend/src/app/modules/monitor/monitor.schedule.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import axios from 'axios';
+import prisma from '../../../lib/prisma';
+import { scheduleJob } from 'node-schedule';
+import { EMonitorStatus } from '@prisma/client';
+import { userAgentHeader } from '../../../lib/headers';
+
+const updateMonitorScheduler = () => {
+  scheduleJob('*/1 * * * *', async () => {
+    const monitors = await prisma.monitor.findMany({
+      orderBy: { createdAt: 'desc' },
+    });
+
+    if (monitors.length === 0) {
+      console.log('No monitors found!');
+      return;
+    }
+
+    try {
+      await axios.get(monitors[0].url, {
+        headers: { 'User-Agent': userAgentHeader },
+      });
+
+      console.log('Schedule job works!');
+    } catch (error: any) {
+      console.error('The Error From Monitor Schedule Job ->', error);
+
+      if (error?.code === 'ENOTFOUND' || error?.code === 'ERR_BAD_RESPONSE') {
+        await prisma.monitor.update({
+          where: { id: monitors[0].id },
+          data: { status: EMonitorStatus.DOWN },
+        });
+      }
+    }
+  });
+};
+
+export const MonitorSchedule = { updateMonitorScheduler };
