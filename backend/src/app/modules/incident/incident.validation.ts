b699641e408b555e3c8a6ef7b0261b1567fcc49a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-27 23:47:44 +0600

    fix: sync zod schema with prisma schema

--- backend/src/app/modules/incident/incident.validation.ts
+++ backend/src/app/modules/incident/incident.validation.ts
@@ -3,10 +3,8 @@ import { z } from 'zod';
 const createAnIncidentZodSchema = z.object({
   body: z.object({
     code: z.string({ required_error: 'Code is required!' }),
-    statusCode: z
-      .number({ required_error: 'Status code is required!' })
-      .positive(),
-    statusMessage: z.string({ required_error: 'Status message is required!' }),
+    statusCode: z.number().positive().optional(),
+    statusMessage: z.string().optional(),
     monitorId: z.string({ required_error: 'Monitor id is required!' }),
   }),
 });
