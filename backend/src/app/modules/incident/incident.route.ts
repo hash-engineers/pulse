cd7eb44349b6af3fd24a87c92ecb9e4a67f9d9b3
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-27 21:37:57 +0600

    feat: add zod validation middleware for incident creation

--- backend/src/app/modules/incident/incident.route.ts
+++ backend/src/app/modules/incident/incident.route.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { IncidentController } from './incident.controller';
+import { IncidentValidation } from './incident.validation';
+import validateZodRequest from '../../middlewares/validate-zod-request';
 
 const router = Router();
 
 router
 
   // WRITE
-  .post('/', IncidentController.createAnIncident);
+  .post(
+    '/',
+    validateZodRequest(IncidentValidation.createAnIncidentZodSchema),
+    IncidentController.createAnIncident,
+  );
 
 export const IncidentRoutes = router;
