8d2e66f7e1b44a25288c4882f4ccd38b9263ab45
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-27 22:16:34 +0600

    feat: update create an incident zod schema

--- backend/src/app/modules/incident/incident.validation.ts
+++ backend/src/app/modules/incident/incident.validation.ts
@@ -2,6 +2,11 @@ import { z } from 'zod';
 
 const createAnIncidentZodSchema = z.object({
   body: z.object({
+    code: z.string({ required_error: 'Code is required!' }),
+    statusCode: z
+      .number({ required_error: 'Status code is required!' })
+      .positive(),
+    statusMessage: z.string({ required_error: 'Status message is required!' }),
     monitorId: z.string({ required_error: 'Monitor id is required!' }),
   }),
 });
