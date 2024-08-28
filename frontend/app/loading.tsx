14bf704de49e34386acfe28fd2a140ba31c08878
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-28 17:56:45 +0600

    feat: add spinner

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -4,28 +4,18 @@ export default function Loading() {
   return (
     <div className="py-8 space-y-4">
       <Skeleton className="w-1/2 h-4" />
-      <Skeleton className="w-full h-3" />
       <Skeleton className="w-full h-[30vh]" />
       <Skeleton className="w-full h-2" />
       <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
         <div className="w-full h-[30vh] space-y-2">
           <Skeleton className="w-full h-[20vh]" />
-          <Skeleton className="w-full h-2" />
-          <Skeleton className="w-full h-2" />
-          <Skeleton className="w-full h-2" />
-          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-full h-3" />
         </div>
         <div className="w-full h-[30vh] space-y-2">
           <Skeleton className="w-full h-[20vh]" />
-          <Skeleton className="w-full h-2" />
-          <Skeleton className="w-full h-2" />
-          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-full h-3" />
         </div>
       </div>
-      <Skeleton className="w-full h-3" />
-      <Skeleton className="w-1/2 h-3" />
-      <Skeleton className="w-3/4 h-2" />
-      <Skeleton className="w-full h-2" />
       <Skeleton className="w-full h-[10vh]" />
     </div>
   );
