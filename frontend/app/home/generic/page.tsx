c43eeb1d2e143cecec876cf1173b9093d53844f2
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-30 18:52:12 +0600

    docs: add generic page

---
+++ frontend/app/home/generic/page.tsx
@@ -0,0 +1,5 @@
+import { UnderConstruction } from '@/components/dummy/under-construction';
+
+export default function Generic() {
+  return <UnderConstruction />;
+}
--- frontend/lib/footer.ts
+++ frontend/lib/footer.ts
@@ -1,38 +1,38 @@
 import { FooterItem, FooterSocialIcon } from '@/types/layout';
 
 const SOLUTIONS_ITEMS: FooterItem[] = [
-  { label: 'Monitoring', href: '/monitoring' },
-  { label: 'Incident', href: '/incident' },
-  { label: 'Dashboards', href: '/dashboards' },
-  { label: 'Integrations', href: '/integrations' },
+  { label: 'Monitoring', href: '/home/generic' },
+  { label: 'Incident', href: '/home/generic' },
+  { label: 'Dashboards', href: '/home/generic' },
+  { label: 'Integrations', href: '/home/generic' },
 ];
 
 const RESOURCES_ITEMS: FooterItem[] = [
-  { label: 'Help & Support', href: '/help-and-support' },
-  { label: 'Docs', href: '/docs' },
-  { label: 'Logs', href: '/logs' },
+  { label: 'Help & Support', href: '/home/generic' },
+  { label: 'Docs', href: '/home/generic' },
+  { label: 'Logs', href: '/home/generic' },
 ];
 
 const COMPANY_ITEMS: FooterItem[] = [
-  { label: 'Work at Pulse', href: '/work-at-pulse' },
-  { label: 'Engineering', href: '/engineering' },
-  { label: 'Security', href: '/security' },
+  { label: 'Work at Pulse', href: '/home/generic' },
+  { label: 'Engineering', href: '/home/generic' },
+  { label: 'Security', href: '/home/generic' },
 ];
 
 const COMMUNITY_ITEMS: FooterItem[] = [
-  { label: 'Guides', href: '/guides' },
-  { label: 'Questions', href: '/questions' },
-  { label: 'Blog', href: '/blog' },
-  { label: 'Comparisons', href: '/comparisons' },
+  { label: 'Guides', href: '/home/generic' },
+  { label: 'Questions', href: '/home/generic' },
+  { label: 'Blog', href: '/home/generic' },
+  { label: 'Comparisons', href: '/home/generic' },
 ];
 
 const FOOTER_SOCIAL_ICONS: FooterSocialIcon[] = [
-  { src: 'instagram', href: '' },
-  { src: 'linkedin', href: '' },
-  { src: 'twitter', href: '' },
-  { src: 'reddit', href: '' },
-  { src: 'github', href: '' },
-  { src: 'youtube', href: '' },
+  { src: 'instagram', href: '/home/generic' },
+  { src: 'linkedin', href: '/home/generic' },
+  { src: 'twitter', href: '/home/generic' },
+  { src: 'reddit', href: '/home/generic' },
+  { src: 'github', href: '/home/generic' },
+  { src: 'youtube', href: '/home/generic' },
 ];
 
 export {
