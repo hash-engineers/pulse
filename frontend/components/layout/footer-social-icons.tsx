6ca41ca0fa7ede474291ab233ffaea7e3bf90985
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-30 18:27:26 +0600

    style: make github icon visible

--- frontend/components/layout/footer-social-icons.tsx
+++ frontend/components/layout/footer-social-icons.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { FooterSocialIcon } from '@/types/layout';
 import { FOOTER_SOCIAL_ICONS } from '@/lib/footer';
+import { cn } from '@/lib/utils';
 
 export function FooterSocialIcons() {
   return (
@@ -8,16 +9,19 @@ export function FooterSocialIcons() {
       <h4 className="text-primary-foreground">Follow Us</h4>
 
       <div className="flex gap-x-6">
-        {FOOTER_SOCIAL_ICONS.map(({ src, href }: FooterSocialIcon) => (
-          <a key={src} href={href}>
-            <Image
-              src={`/images/social/${src}.png`}
-              alt="Social Icon"
-              height={28}
-              width={28}
-            />
-          </a>
-        ))}
+        {FOOTER_SOCIAL_ICONS.map(
+          ({ src, href }: FooterSocialIcon, i: number) => (
+            <a key={src} href={href}>
+              <Image
+                src={`/images/social/${src}.png`}
+                alt="Social Icon"
+                height={28}
+                width={28}
+                className={cn({ 'bg-white rounded-full': i === 4 })}
+              />
+            </a>
+          )
+        )}
       </div>
     </div>
   );
