547a180471f8729093bee300d252ca91b566ae21
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-01 21:36:02 +0600

    fix: handle access to dashboard

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from 'react';
+import { auth } from '@/lib/paths/auth';
 import { redirect } from 'next/navigation';
+import { getAnUserById } from '@/actions/user';
+import { dashboard } from '@/lib/paths/dashboard';
 import { Sidebar } from '@/components/layout/sidebar';
 import { OpenSidebar } from '@/components/layout/open-sidebar';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
@@ -8,8 +11,13 @@ type Props = { children: ReactNode };
 
 export default async function DashboardLayout({ children }: Props) {
   const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-  if (!(await getUser())) redirect('/api/auth/login');
+  if (!user) redirect(auth.login.path);
+
+  const dbUser = await getAnUserById({ id: user.id });
+
+  if (!dbUser) redirect(dashboard.companies.createCompany.path);
 
   return (
     <div className="relative h-screen">
